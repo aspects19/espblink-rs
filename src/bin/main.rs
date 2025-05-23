f8fe0b3f5692b2eb9978fa3732a5d6a89cf98d1a
aspect19 <jeffamenya388@gmail.com>
2025-05-23 10:04:51 +0300

    Working code

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,3 +1,26 @@
 #![no_std]
 #![no_main]
 
+use esp_backtrace as _;
+use esp_hal::{
+    clock::CpuClock, 
+    gpio::{ Level, Output, OutputConfig}, 
+    main, 
+    time::{Duration, Instant}
+};
+
+
+#[main]
+fn main() -> ! {
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    let peripherals = esp_hal::init(config);
+
+    let mut led = Output::new(peripherals.GPIO2, Level::High, OutputConfig::default());
+
+    loop {
+        led.toggle();
+
+        let delay_start = Instant::now();
+        while delay_start.elapsed() < Duration::from_millis(500) {}
+    }
+}
